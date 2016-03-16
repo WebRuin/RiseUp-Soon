7665daed005405ce5d089397ca71549918d382e4
Tim Smith <tim.smith.hdg@gmail.com>
2016-03-15 19:50:47 -0700

    contact form

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -66,6 +66,7 @@ $( document ).ready(function() {
       $('.menu-button-dropdown').css('display', 'none');
     }
   );
+  // END Menu Trigger
 
   // Scroll Fuctions
   $(window).scroll(function() {
@@ -87,6 +88,7 @@ $( document ).ready(function() {
       }
     );
   });
+  // END Scroll Fuctions
 
   // Scroll summary grid to top when you click on it
   $('.summary-grid').on('click', '*', function() {
@@ -94,6 +96,7 @@ $( document ).ready(function() {
         scrollTop: $(".container-body").offset().top
     }, 2000);
   });
+  // END Scroll summary grid to top when you click on it
 
   // Summary grid tab switching
   $('ul.summary-grid li').click(function(){
@@ -116,6 +119,37 @@ $( document ).ready(function() {
       }
     }
 	})
+  // END Summary grid tab switching
+
+  // Contact Form
+   var form = document.getElementsByTagName('form')[0];
+   form.addEventListener('submit',contact,false);
+   function contact(e) {
+      // Prevent Default Form Submission
+      e.preventDefault();
+
+      var target = e.target || e.srcElement;
+      var i = 0;
+      var message = '';
+
+      // Loop Through All Input Fields
+      for(i = 0; i < target.length; ++i) {
+         // Check to make sure it's a value. Don't need to include Buttons
+         if(target[i].type != 'text' && target[i].type != 'textarea') {
+                // Skip to next input since this one doesn't match our rules
+            continue;
+         }
+
+         // Add Input Name and value followed by a line break
+         message += target[i].name + ': ' + target[i].value + "\r\n";
+      }
+      // Modify the hidden body input field that is required for the mailto: scheme
+      target.elements["body"].value = message;
+
+      // Submit the form since we previously stopped it. May cause recursive loop in some browsers? Should research this.
+      this.submit();
+   }
+   // END Contact Form
 
 });
 
